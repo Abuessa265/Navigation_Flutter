a433bb11e8e0388b48ce6706fbc747a013afc315
Abuessa265 <abuessastudent1999@gmail.com>
2024-03-27 21:48:14 +0600

    value pass

--- lib/main.dart
+++ lib/main.dart
@@ -33,34 +33,42 @@ class HomeActivity extends StatelessWidget{
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1()));
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1("This is from home to activity 1")));
             }, child: Text("Go Activity 1")),
             SizedBox(height: 20,width: 20,),
             ElevatedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2()));
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2("This is from home to activity 2")));
             }, child: Text("Go Activity 2"))
           ],
         ),
-
-
       ),
     );
   }
 }
 
 class Activity1 extends StatelessWidget{
+
+  String msg;
+
+   Activity1(
+      this.msg,
+      {super.key
+      });
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
       appBar: AppBar(
         backgroundColor: Colors.cyan,
-        title: Text('Activity1'),
+        title: Text(msg),
       ),
       body: Center(
 
         child: ElevatedButton(onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2()));
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2('This is from activity 1 to activity 2')));
         },child: Text('Go Activity 2'),),
       ),
     );
@@ -68,17 +76,24 @@ class Activity1 extends StatelessWidget{
 }
 
 class Activity2 extends StatelessWidget{
+
+  String msg;
+
+   Activity2(
+       this.msg,
+       {super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
       appBar: AppBar(
         backgroundColor: Colors.cyan,
-        title: Text('Activity2'),
+        title: Text(msg),
       ),
       body: Center(
         child: ElevatedButton(onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1()));
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1("This is from activity 2 to activity 1")));
         },child: Text("Go activity 1"),),
       ),
     );
